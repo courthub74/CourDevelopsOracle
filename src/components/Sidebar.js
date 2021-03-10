3b3d2d4cc0408681b0f19213e4c8d1307df3fcb3
CourDevelops <courdevelops@gmail.com>
2021-03-09 20:49:30 -0500

    Hamburger no interactivity

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import { SidebarData } from './SidebarData';
+import { IconContext } from 'react-icons/lib';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import './Sidebar.css';
 // import SubMenu from './Submenu';
 import Scrollbars from 'react-custom-scrollbars';
+import * as FaIcons from 'react-icons/fa';
 
+//ADD NAVBAR TO TOP
+const Nav = styled.div`
+    background: linear-gradient(90deg, rgb(19, 18, 22) 20%, rgb(23, 13, 204) 100%);
+    height: 100px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+`;
+
+//NAVBURGER
+const NavIcon = styled(Link)`
+    margin-left: 2rem;
+    font-size: 2rem;
+    height: 80px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+`;
 
 //SIDEBAR
 const SidebarNav = styled.nav`
-    background: #15172c;
+    background: #09090B;
     width: 300px;
     height: 100vh;
     display: flex;
@@ -30,26 +50,36 @@ const SidebarWrap = styled.div`
 export const Sidebar = () => {
     return (
         <>
-            <SidebarNav>
-                
-                <SidebarWrap className="SidebarWrap">
-                    <Scrollbars style={{ width: 300, height: 1000 }}>
-                        Hello
-                        Map 
-                        Sidebar Data here 🍩 
-                        {SidebarData.map((item, index) => {
-                            return (
-                                <li key={index} className={item.cName}>
-                                    <Link to={item.path}>
-                                        {item.icon}
-                                        <span>{item.title}</span>
-                                    </Link>
-                                </li>
-                            )
-                        })}
-                    </Scrollbars>
-                </SidebarWrap>
-            </SidebarNav>
+
+            <IconContext.Provider value={{ color: '#fff' }}>
+
+                <Nav>
+                    <NavIcon to='#'>
+                        <FaIcons.FaBars />
+                    </NavIcon>
+                </Nav>
+
+                    <SidebarNav>
+                        
+                        <SidebarWrap className="SidebarWrap">
+                            <Scrollbars style={{ width: 300, height: 1000 }}>
+                                Hello
+                                Map 
+                                Sidebar Data here 🍩 
+                                {SidebarData.map((item, index) => {
+                                    return (
+                                        <li key={index} className={item.cName}>
+                                            <Link to={item.path}>
+                                                {item.icon}
+                                                <span>{item.title}</span>
+                                            </Link>
+                                        </li>
+                                    )
+                                })}
+                            </Scrollbars>
+                        </SidebarWrap>
+                    </SidebarNav>
+            </IconContext.Provider> 
         </>
     );
 };
