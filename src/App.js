bc358f6dda01be3d43eeca9a7277f5c56d815cc4
CourDevelops <courdevelops@gmail.com>
2021-03-13 20:28:48 -0500

    Page Mapping started

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Python } from './pages/Python';
 import { Javascript } from './pages/Javascript';
 import { Projects } from './pages/Projects';
 import { PYFunctions } from './pages/PYFunctions';
+import { SelfParameter } from './pages/Python/SelfParameter';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
             <Route path='/pages/Home' exact component={Home} />
             <Route path='/python' exact component={Python} />
             <Route path='/python/functions' exact component={PYFunctions} />
+            <Route path='/python/self' exact component={SelfParameter} />
             <Route path='/javascript' exact component={Javascript} />
             <Route path='/pages/projects' exact component={Projects} />
           </Switch>
--- src/pages/PYFunctions.js
+++ src/pages/PYFunctions.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Card } from 'react-bootstrap'
+import './pages.css'
+import * as SiIcons from 'react-icons/si';
 
 export const PYFunctions = () => {
     return (
         <div className='pyfunctions'>
-            Python Functions Here
-        </div>
+            <Card className='card'>
+            <Card.Header className="center"><h1>Functions <SiIcons.SiPython /></h1>
+            </Card.Header>
+            <Card.Body>
+                <Card.Text className="texty">
+                    A function is a block of code which only runs when it is called.<br></br>
+
+                    You can pass data, known as parameters, into a function.<br></br>
+
+                    A function can return data as a result.
+                </Card.Text>
+                <Card.Text className="bold">
+                <span>def my_function():</span><br></br>
+                    <span className='tab5'>print("Hello from a function")</span>
+                </Card.Text>
+            </Card.Body>
+            </Card>
+        </div>   
     )
 }
 
